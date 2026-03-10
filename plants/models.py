95f3ca65791b638b5c3e9acae5141a8780877270
Siyana Trendafilova <bebovska@gmail.com>
2026-03-11 00:26:46 +0200

    feat: Add care frequency to Plant model

--- plants/models.py
+++ plants/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
 from plants.validators import PlantNameValidator
@@ -34,7 +34,6 @@ class Plant(models.Model):
         null=True,
         blank=True,
     )
-
     description = models.TextField()
 
     image_url = models.URLField(
@@ -45,32 +44,50 @@ class Plant(models.Model):
         max_length=50,
         help_text='*The city where the plant is located.'
     )
-
     address = models.CharField(
         max_length=100,
         blank=True,
         null=True,
         help_text='*Optional: apartment, floor, or specific spot (e.g., "Balcony").'
     )
-
     created_at = models.DateTimeField(
         auto_now_add=True,
     )
-
     gardener = models.ForeignKey(
         to='gardeners.Gardener',
         on_delete=models.CASCADE,
         related_name='plants',
     )
-
     tags = models.ManyToManyField(
         to='Tag',
         related_name='plants',
         blank=True,
+    )
+    watering_frequency = models.PositiveIntegerField(
+        default=1,
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(60),
+        ],
+        help_text='*Share how many times per month the plant needs watering.',
+    )
+    fertilizing_frequency = models.PositiveIntegerField(
+        default=0,
+        validators=[
+            MaxValueValidator(24),
+        ],
+        help_text='*How many times per year should this plant be fertilized?'
+    )
+    pruning_frequency = models.PositiveIntegerField(
+        default=0,
+        validators=[
+            MaxValueValidator(12),
+        ],
+        help_text='*How many times per year should this plant be pruned?'
     )
 
     class Meta:
         ordering = ['plant_name']
 
     def __str__(self):
-        return self.plant_name
+        return f'{self.plant_name} - {self.city} ({self.address})'
