c651f0eae54c80ab8f910acc3c4c6c33742a394b
Siyana Trendafilova <bebovska@gmail.com>
2026-03-10 04:30:48 +0200

    feat: Add Gardener model and custom validators

--- gardeners/models.py
+++ gardeners/models.py
@@ -1,3 +1,61 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+from gardeners.validators import LettersDigitsOnlyValidator, FirstAndLastNameValidator
+
+
+class Gardener(models.Model):
+    nickname = models.CharField(
+        max_length=40,
+        validators=[
+            MinLengthValidator(3, 'Your nickname must be at least 3 chars long!'),
+            LettersDigitsOnlyValidator(),
+        ],
+        unique=True,
+        error_messages={
+            'unique': 'That nickname is already in use!'
+        },
+        help_text='*Allowed nicknames can contain only letters and digits.',
+    )
+    first_name = models.CharField(
+        max_length=40,
+        validators=[
+            MinLengthValidator(2, 'Your name must be at least 2 chars long!'),
+            FirstAndLastNameValidator(),
+        ],
+        help_text='*Starts with a capital letter. Letters and hyphens only.',
+    )
+    last_name = models.CharField(
+        max_length=40,
+        validators=[
+            MinLengthValidator(2, 'Your name must be at least 2 chars long!'),
+            FirstAndLastNameValidator(),
+        ],
+        help_text='*Starts with a capital letter. Letters and hyphens only.',
+    )
+    email = models.EmailField(
+        max_length=40,
+        unique=True,
+        error_messages={
+            'unique': 'That email is already registered!'
+        },
+    )
+    about_me = models.TextField(
+        null=True,
+        blank=True,
+    )
+    profile_picture = models.URLField(
+        null=True,
+        blank=True,
+    )
+    is_pro = models.BooleanField(
+        default=False,
+    )
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.nickname
+
---
+++ gardeners/validators.py
@@ -0,0 +1,26 @@
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+
+
+@deconstructible
+class LettersDigitsOnlyValidator:
+    def __init__(self, message: str = 'A nickname must contain only letters and digits!'):
+        self.message = message
+
+    def __call__(self, value: str, *args, **kwargs):
+        if not value.isalnum():
+            raise ValidationError(self.message)
+
+
+@deconstructible
+class FirstAndLastNameValidator:
+    def __init__(self, message: str = 'Name must start with a capital letter and contain only letters and hyphens!'):
+        self.message = message
+
+    def __call__(self, value: str, *args, **kwargs):
+        if not value[0].isupper():
+            raise ValidationError(self.message)
+
+        for char in value:
+            if not (char.isalpha() or char == '-'):
+                raise ValidationError(self.message)
