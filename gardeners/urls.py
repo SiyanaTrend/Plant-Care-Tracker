672ef223936dfddc1299a2112938a608294e0157
Siyana Trendafilova <bebovska@gmail.com>
2026-03-13 03:52:51 +0200

    feat: Add profile details view and urls

--- gardeners/urls.py
+++ gardeners/urls.py
@@ -4,5 +4,6 @@ from gardeners import views
 
 urlpatterns = [
     path('', views.GardenerCreateView.as_view(), name='create-gardener'),
+    path('details/', views.GardenerDetailsView.as_view(), name='gardener-details'),
     path('edit/', views.GardenerEditView.as_view(), name='edit-gardener'),
 ]
--- gardeners/views.py
+++ gardeners/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from common.mixins import SingleProfileMixin
 from gardeners.forms import GardenerCreateForm, GardenerEditForm
@@ -16,9 +16,13 @@ class GardenerCreateView(CreateView):
         return super().form_valid(form)
 
 
+class GardenerDetailsView(SingleProfileMixin, DetailView):
+    model = Gardener
+    template_name = 'gardeners/gardener-details.html'
+
+
 class GardenerEditView(SingleProfileMixin, UpdateView):
     model = Gardener
     form_class = GardenerEditForm
     template_name = 'gardeners/edit-gardener.html'
     success_url = reverse_lazy('home')
-
