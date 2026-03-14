10592a49306aab64bb01951851b7f33e27ab99ca
Siyana Trendafilova <bebovska@gmail.com>
2026-03-14 09:53:05 +0200

    feat(urls): Add urls for Plant model

--- plants/urls.py
+++ plants/urls.py
@@ -4,4 +4,10 @@ from plants import views
 
 urlpatterns = [
     path('', views.PlantsListView.as_view(), name='catalogue'),
+    path('create/', views.PlantCreateView.as_view(), name='create-plant'),
+    path('<int:plant_pk>/', include([
+        path('details/', views.PlantDetailsView.as_view(), name='plant-details'),
+        path('edit/', views.PlantEditView.as_view(), name='edit-plant'),
+        path('delete/', views.PlantDeleteView.as_view(), name='delete-plant'),
+    ])),
 ]
