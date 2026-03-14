4b580f86c43ced8c37d39e4825da75c7a0eff4cd
Siyana Trendafilova <bebovska@gmail.com>
2026-03-14 09:43:04 +0200

    feat: Add mixin for hiding help_texts

--- common/mixins.py
+++ common/mixins.py
@@ -4,3 +4,19 @@ from common.utils import get_profile
 class SingleProfileMixin:
     def get_object(self, queryset=None):
         return get_profile()
+
+
+class ReadOnlyMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs['readonly'] = True
+
+
+class HiddenHelpText:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.help_text = ''
