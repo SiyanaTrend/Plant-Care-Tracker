b6a35fcf3b364d13cfc51f5f1f6131bbb195e764
Siyana Trendafilova <bebovska@gmail.com>
2026-03-17 01:28:02 +0200

    feat: Add statistics view and urls

--- common/urls.py
+++ common/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from common import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home')
+    path('', views.HomePageView.as_view(), name='home'),
+    path('statistics/', views.statistics_view, name='statistics'),
 ]
--- common/views.py
+++ common/views.py
@@ -1,6 +1,39 @@
+from django.db.models import Count, Max
+from django.shortcuts import render
 from django.views.generic import TemplateView
 from common.mixins import SingleProfileMixin
+from maintenance.models import MaintenanceRecord
+from plants.models import Plant, Tag
 
 
 class HomePageView(SingleProfileMixin, TemplateView):
     template_name = 'common/home.html'
+
+
+def statistics_view(request):
+    all_plants = Plant.objects.all()
+    total_records = MaintenanceRecord.objects.count()
+
+    max_actions = all_plants.annotate(
+        actions=Count('maintenance_records')
+    ).aggregate(Max('actions'))['actions__max']
+
+    star_plants = []
+    if max_actions and max_actions > 0:
+        star_plants = all_plants.annotate(
+            actions=Count('maintenance_records')
+        ).filter(actions=max_actions)
+
+    top_tags = Tag.objects.annotate(
+        count=Count('plants')
+    ).filter(count__gt=0).order_by('-count')[:3]
+
+    context = {
+        'total_plants': all_plants.count(),
+        'total_records': total_records,
+        'top_tags': top_tags,
+        'star_plants': star_plants,
+        'max_actions': max_actions,
+        'all_plants': all_plants,
+    }
+    return render(request, 'common/statistics.html', context)
