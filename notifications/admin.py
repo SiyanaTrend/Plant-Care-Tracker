36e009881b98b9b5bd75c7bee64521f581c3d5df
Siyana Trendafilova <bebovska@gmail.com>
2026-04-05 21:43:04 +0300

    chore: Register Notification model in django admin

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from notifications.models import Notification
 
-# Register your models here.
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'plant', 'action_type', 'is_read', 'created_at')
+    list_filter = ('is_read', 'action_type')
+
+    readonly_fields = ('user', 'plant', 'action_type', 'created_at')
