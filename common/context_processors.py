017f42f3b5f7e45851171cc4570403dbcfbddf16
Siyana Trendafilova <bebovska@gmail.com>
2026-04-06 21:25:10 +0300

    chore: Optimize notifications functionality

--- common/context_processors.py
+++ common/context_processors.py
@@ -3,6 +3,6 @@ from notifications.models import Notification
 
 def notification_count(request):
     if request.user.is_authenticated:
-        count = Notification.objects.filter(user=request.user, is_read=False).count()
+        count = Notification.objects.filter(user=request.user).count()
         return {'unread_count': count}
     return {'unread_count': 0}
