f2a6bf6aa877b56f731fd366f79f9bf68d06fef5
Siyana Trendafilova <bebovska@gmail.com>
2026-04-07 10:10:34 +0300

    fix. Update settings.py

--- plantCareTracker/settings.py
+++ plantCareTracker/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
+# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
 
 STORAGES = {
     "default": {
@@ -163,7 +163,7 @@ if 'test' in sys.argv:
         },
     }
 else:
-    STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
+    # STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
     STORAGES = {
         "default": {
             "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
